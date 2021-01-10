aa532268d38cd37ef62a08ac3d9b9924fd422844
Michal Charemza <michal@charemza.name>
2021-01-10 06:49:07 +0000

    feat: simpler generation of cert, without needing passphrase

--- generate-self-signed-cert.sh
+++ generate-self-signed-cert.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-openssl genrsa -des3 -out server.key 2048
-openssl rsa -in server.key -out server.key
-openssl req -new -key server.key -days 3650 -out server.crt -x509 -subj "/CN=Common Name"
+openssl req  -nodes -new -x509 -subj "/CN=localhost" -keyout server.key -out server.cert
