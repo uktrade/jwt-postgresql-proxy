cac7a06f57a3f8a93e26abd834c047c5963c1dc7
Michal Charemza <michal@charemza.name>
2021-01-11 20:17:20 +0000

    tests: startup application in tests

--- test.py
+++ test.py
@@ -1,7 +1,48 @@
+import os
+import socket
+import subprocess
+import time
 import unittest
+
+
+def with_application():
+    def decorator(original_test):
+        def test_with_application(self):
+            process = subprocess.Popen(
+                ['python3', '-m', 'jwt_postgresql_proxy'],
+                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                env={
+                    **os.environ,
+                }
+            )
+
+            def stop():
+                process.kill()
+                process.wait(timeout=5)
+                process.stderr.close()
+                process.stdout.close()
+
+            try:
+                for i in range(0, 100):
+                    try:
+                        with socket.create_connection(('127.0.0.1', '7777'), timeout=0.1):
+                            break
+                    except (OSError, ConnectionRefusedError):
+                        if i == 100 - 1:
+                            raise
+                        time.sleep(0.02)
+                original_test(self)
+            finally:
+                stop()
+
+        return test_with_application
+
+    return decorator
 
 
 class TestProxy(unittest.TestCase):
 
+    @with_application()
     def test_dummy(self):
         self.assertTrue(1 > 0)
